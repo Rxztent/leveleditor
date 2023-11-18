ac58056147b795ca896f1ac9cc35afbbd04ea7fd
Rxztent <49635832+Rxztent@users.noreply.github.com>
2023-11-18 10:31:28 +0100

    Update modloadercode.js

--- modloadercode.js
+++ modloadercode.js
@@ -1172,7 +1172,7 @@ function simulateClickOnGameBoard() {
 
 window.levelEditorMod.alterSnakeCode = function(code) {
   code = code.replaceAll(/\$\$/gm, `aaaa`); //Prevent issues with $$ in variable names breaking stuff when replaced
-  print(code);
+  console.log(code);
   // altersnake
   ///////////////////////////////////////
   //Taken from shared.js
